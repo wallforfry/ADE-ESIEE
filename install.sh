47da56f89dbc0f3aa43f0fe55a3573fc0635e367
Wallerand Delevacq <wallerand@template.ovh>
2017-04-18 01:22:23 +0200

    add script for install and daemon script

---
+++ install.sh
@@ -0,0 +1,3 @@
+/usr/bin/python3 get-pip.py
+/usr/bin/python3 -m pip install requests
+/usr/bin/python3 -m pip install flask
---
+++ run-ade.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+script='/home/wallerand/ade-esiee/main.py'
+/usr/bin/python3 $script &
